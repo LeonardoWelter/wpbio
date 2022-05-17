f1c0ef884fc171873837d5962c94ed6b3ecd504c
LeonardoWelter <leonardo.dias_silva@hotmail.com>
2022-05-17 16:32:23 -0300

    * Update file;

--- public/lwbio_links_template.php
+++ public/lwbio_links_template.php
@@ -1,40 +1,34 @@
 <?php //WP Links Template
 
-    $links = LW_DB::index(0);
-    $channels = LW_DB::index(1);
-    $logo = esc_attr( get_option('lwbio_logo') );
+$links = BioDatabase::index(0);
+$channels = BioDatabase::index(1);
+$logo = esc_attr(get_option('lwbio_logo'));
+$channel_text = esc_attr(get_option('lwbio_channel_text'));
 
 ?>
 <section id="lwbio__outer-card">
     <div id="lwbio__card">
-        <?php 
-            if (!empty($logo)) {
-                echo "<img id='lwbio__logo' src='{$logo}' alt='logo' />";
-            } else {
-                echo "<i id='lwbio__temp_logo' class='bi bi-globe2' style='font-size: 128px;'></i>";
-            }
-        ?>
+        <?php if (!empty($logo)) : ?>
+            <img id='lwbio__logo' src='<?= $logo ?>' alt='logo' />
+        <?php else: ?>
+            <i id='lwbio__temp_logo' class='bi bi-globe2' style='font-size: 128px;'></i>
+        <?php endif; ?>
+
         <div class="lwbio__links">
-            <?php
-                foreach($links as $value) {
-                    echo "<div class='lwbio__links__item'>
-                                 <a class='lwbio__links__item lwbio__links__item--lb' href='{$value->url}' 
-                                     target='_blank' rel='noopener'>{$value->text}
-                                 </a>
-                            </div>";
-                }                                       
-            ?>
+            <?php foreach ($links as $value) : ?>
+                <div class='lwbio__links__item'>
+                    <a class='lwbio__links__item lwbio__links__item--lb' href='<?= $value->url ?>' target='_blank' rel='noopener'><?= $value->text ?>
+                    </a>
+                </div>
+            <?php endforeach; ?>
         </div>
-        <h2 id="lwbio__h2">Me siga nas redes sociais</h2>
+        <h2 id="lwbio__h2"><?= empty($channel_text) ? 'Me siga nas redes sociais' : $channel_text ?></h2>
         <div class="lwbio__channels">
-            <?php
-                foreach($channels as $value) {
-                        echo "<a class='lwbio__channels__item' 
-                                href='{$value->url}' target='_blank' rel='noopener'>
-                                <i class='". lwbio_fa_icon($value->service)."'></i>
-                              </a>";
-                }
-            ?>
+            <?php foreach ($channels as $value) : ?>
+                <a class='lwbio__channels__item' href='<?= $value->url ?>' target='_blank' rel='noopener'>
+                    <i class='<?= lwbio_fa_icon($value->service) ?>'></i>
+                </a>
+            <?php endforeach; ?>
         </div>
-</div>
+    </div>
 </section>
