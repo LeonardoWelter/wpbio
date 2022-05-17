fa6261193e6589a33ab1e817c9a252b6d923fc60
LeonardoWelter <leonardo.dias_silva@hotmail.com>
2022-05-17 16:29:06 -0300

    * Update filename and contents;

--- wpbio.php
+++ wpbio.php
@@ -12,7 +12,7 @@
  * Plugin Name:       WP Bio
  * Plugin URI:        https://github.com/LeonardoWelter/WPBio
  * Description:       Keep all your links in one place.
- * Version:           1.2.0
+ * Version:           2.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Leonardo Welter
@@ -26,12 +26,11 @@
 
 defined('ABSPATH') or die('Nope, not accessing this');
 
-require_once("includes/db.php");
-require_once("includes/lwbio_icon_handler.php");
+require_once("src/BioDatabase.php");
+require_once("src/IconHandler.php");
 require_once("admin/admin.php");
 
-function lwbio_links()
-{
+function lwbio_links() {
 	ob_start();
 	include dirname(__FILE__) . '/public/lwbio_links_template.php';
 	$links_template = ob_get_contents();
@@ -42,19 +41,24 @@ function lwbio_links()
 
 add_shortcode('lwbio-links', 'lwbio_links');
 
-function lwbio_enqueue()
-{
+function lwbio_enqueue() {
 	wp_enqueue_style('lwbio_bi', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css');
 	wp_enqueue_style('lwbio', plugins_url('public/includes/css/lwbio_links.css', __FILE__));
 }
 
-function register_lwbio_setting() {
-	register_setting( 'lwbio', 'lwbio_logo' );
+function lwbio_admin_enqueue() {
+	wp_enqueue_style('lwrbootstrapicons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css');
+	wp_enqueue_style('lwrbootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+	wp_enqueue_script('lwrbootstrapjs', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js');
 }
 
-LW_DB::getInstance();
+function register_lwbio_setting() {
+	register_setting('lwbio', 'lwbio_logo');
+	register_setting('lwbio', 'lwbio_channel_text');
+}
 
-register_activation_hook(__FILE__, ['LW_DB', 'install']);
-register_activation_hook(__FILE__, ['LW_DB', 'install_data']);
+register_activation_hook(__FILE__, ['BioDatabase', 'install']);
+register_activation_hook(__FILE__, ['BioDatabase', 'install_data']);
 add_action('wp_enqueue_scripts', 'lwbio_enqueue');
+add_action('admin_enqueue_scripts', 'lwbio_admin_enqueue', 0);
 add_action('admin_init', 'register_lwbio_setting');
