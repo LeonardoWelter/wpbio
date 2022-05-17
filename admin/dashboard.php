66157d763735d6b4eef5333e187418e909fa738a
LeonardoWelter <leonardo.dias_silva@hotmail.com>
2022-05-17 16:32:43 -0300

    * Update file;

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,63 +1,114 @@
-<?php ?>
+<?php
+$linksAll = BioDatabase::index()
+?>
 <div class="wrap">
-    <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+    <h1><?= esc_html(get_admin_page_title()); ?></h1>
     <p style="text-align: center;">Precisa de ajuda sobre o plugin? Visite a <a href="https://wpbiodocs.readthedocs.io/en/latest/index.html">documentação</a> do plugin!</p>
-    <div id="lwbio_action_bar">
-        <a href="<?php echo admin_url('admin.php?page=lwbio_add'); ?>" class="lwbio_action_bar_button">Adicionar Link</a>
+    <div class="text-end">
+        <a href="<?= admin_url('admin.php?page=lwbio_add'); ?>" class="btn btn-primary">Adicionar Link</a>
     </div>
-    <table class="lwbio_table">
-        <tr>
-            <td colspan="3" class="lwbio_table_title">Botões</td>
-        </tr>
+    <h4 class="text-center">Links</h4>
+    <table class="table table-striped">
         <tr>
             <th>Texto</th>
             <th>Link</th>
             <th>Ações</th>
         </tr>
-        <?php  
-            foreach(LW_DB::index(0) as $item) {
-                echo "<tr>
-						<td class='lwbio_td_text'>{$item->text}</td>
-						<td class='lwbio_td_url'>{$item->url}</td>
-						<td class='lwbio_td_actions'>
-							<a href='".admin_url("admin.php?page=lwbio_edit&id={$item->id}")."' class='lwbio_td_actions_button'><i class='bi bi-pencil-square'></i></a>
-							<form action=".admin_url('admin-post.php')." method='post' class='lwbio_table_form'>
-                                <input type='hidden' name='action' value='remove_link'>
-                                <input type='hidden' name='data' value='link'>
-                                <input type='hidden' name='lwbio_id' value=".$item->id.">
- 								<button class='lwbio_table_form_button' type='submit'><i class='bi bi-trash3-fill'></i></button>
-							</form>
-						</td>
-					</tr>";
-            }
-        ?>
+        <?php foreach (BioDatabase::index(0) as $item) : ?>
+            <tr>
+                <td style="min-width: 45%; max-width: 45%;"><?= $item->text ?></td>
+                <td style="min-width: 45%; max-width: 45%;"><?= $item->url ?></td>
+                <td class="btn-group w-100" role='group'>
+                    <div class="w-100">
+                        <a href='<?= admin_url("admin.php?page=lwbio_edit&id={$item->id}") ?>' class='btn btn-primary'><i class='bi bi-pencil-square'></i></a>
+                        <button class="btn btn-danger" onclick="confirmModal(<?= $item->id ?>,'remove_link')"><i class='bi bi-trash3-fill'></i></button>
+                    </div>
+                </td>
+            </tr>
+        <?php endforeach; ?>
     </table>
 
-    <table class="lwbio_table">
+    <h4 class="text-center mt-5">Redes Sociais</h4>
+    <table class="table table-striped">
         <tr>
-            <td colspan="3" class="lwbio_table_title">Redes Sociais</td>
+            <th scope="col">Serviço</th>
+            <th scope="col">Link</th>
+            <th scope="col">Ações</th>
         </tr>
-        <tr>
-            <th>Serviço</th>
-            <th>Link</th>
-            <th>Ações</th>
-        </tr>
-    <?php
-        foreach(LW_DB::index(1) as $item) {
-            echo "<tr>
-					<td class='lwbio_td_service'>{$item->service}</td>
-					<td class='lwbio_td_url'>{$item->url}</td>
-					<td class='lwbio_td_actions'>
-						<a href='".admin_url("admin.php?page=lwbio_edit&id={$item->id}")."' class='lwbio_td_actions_button'><i class='bi bi-pencil-square'></i></a>
-						<form action=".admin_url('admin-post.php')." method='post' class='lwbio_table_form'>
-                            <input type='hidden' name='action' value='remove_link'>
-                            <input type='hidden' name='data' value='link'>
-                            <input type='hidden' name='lwbio_id' value=".$item->id.">
-							<button class='lwbio_table_form_button' type='submit'><i class='bi bi-trash3-fill'></i></button>
-						</form>
-					</td>
-				</tr>";
-        }
-    ?>
+        <?php foreach (BioDatabase::index(1) as $item) : ?>
+            <tr>
+                <td style="min-width: 45%; max-width: 45%;"><?= ucfirst($item->service) ?></td>
+                <td style="min-width: 45%; max-width: 45%;"><?= $item->url ?></td>
+                <td>
+                    <a href='<?= admin_url("admin.php?page=lwbio_edit&id={$item->id}") ?>' class='btn btn-primary'><i class='bi bi-pencil-square'></i></a>
+                    <button class="btn btn-danger" onclick="confirmModal(<?= $item->id ?>,'remove_link')"><i class='bi bi-trash3-fill'></i></button>
+                </td>
+            </tr>
+        <?php endforeach; ?>
     </table>
 </div>
+
+<!-- Button trigger modal -->
+<button id="launchModal" type="button" data-bs-toggle="modal" data-bs-target="#confirmModal" hidden></button>
+
+<!-- Modal -->
+<div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmModalLabel">Remover pedido?</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="confirmModalBody">
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <form action="<?= admin_url('admin-post.php') ?>" method='post' class='d-inline'>
+                    <input id="formAction" type='hidden' name='action' value=''>
+                    <input id="formId" type='hidden' name='lwbio_id' value=''>
+                    <input id="formBtn" type="submit" class="btn" value="Confirmar">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    var js_data = '<?php echo json_encode($linksAll); ?>';
+    js_data = JSON.parse(js_data);
+
+
+    function confirmModal(id, action) {
+
+        let link = js_data.find(o => o.id == id);
+
+        type = link.type == 0 ? 'Link' : 'Rede Social';
+
+        modalBody = document.getElementById('confirmModalBody');
+        modalBody.innerHTML = `<p>Texto: <br> ${link.text}</p>
+                                <p>Link: <br> ${link.url}</p>
+                                <p>Tipo: <br> ${type}</p>
+                                <p>Serviço:  <br> ${link.service}</p>`;
+
+        formAction = document.getElementById('formAction');
+        formAction.value = action;
+
+        formId = document.getElementById('formId');
+        formId.value = id;
+
+        switch (action) {
+            case 'remove_link':
+                modalLabel = document.getElementById('confirmModalLabel');
+                modalLabel.innerHTML = "Remover pedido?"
+
+                formBtn = document.getElementById('formBtn');
+                formBtn.className = 'btn btn-danger';
+                formBtn.value = 'Remover';
+                break;
+        }
+
+        document.getElementById('launchModal').click();
+
+    }
+</script>
