bab116c7e25b2d1d2d9df66ac9b05317d79ef0e1
LeonardoWelter <leonardo.dias_silva@hotmail.com>
2022-05-17 16:33:13 -0300

    * Switch CSS to Bootstrap; * Add Channel Text option;

--- admin/config.php
+++ admin/config.php
@@ -1,13 +1,21 @@
 <?php ?>
 <div class="wrap">
-	<h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-	<div id="lwbio_form_link" class="lwbio_form_card">
-		<form method="post" action="options.php" class="lwbio_form">
-			<?php settings_fields( 'lwbio' ); ?>
-    		<?php do_settings_sections( 'lwbio' ); ?>
-			<label for="text">URL do Logotipo</label>
-			<input type="text" name="lwbio_logo" id="url" placeholder="Insira a URL" value="<?php echo esc_attr( get_option('lwbio_logo') ); ?>">
-			<button type="submit" class="lwbio_button_submit">Salvar configurações</button>
-		</form>
+	<div class="card w-75 mt-5 text-center mx-auto">
+		<div class="card-body p-5">
+			<h5 class="card-title">Configurações</h5>
+			<form method="post" action="options.php" class="lwbio_form">
+				<?php settings_fields('lwbio'); ?>
+				<?php do_settings_sections('lwbio'); ?>
+				<div class="mb-3 mt-3">
+					<label class="form-label" for="lwbio_logo">URL do Logotipo</label>
+					<input class="form-control" type="text" name="lwbio_logo" id="lwbio_logo" placeholder="Insira a URL" value="<?php echo esc_attr(get_option('lwbio_logo')); ?>">
+				</div>
+				<div class="mb-3 mt-3">
+					<label class="form-label" for="lwbio_channel_text">Texto Redes Sociais</label>
+					<input class="form-control" type="text" name="lwbio_channel_text" id="lwbio_channel_text" placeholder="Insira o texto" value="<?php echo esc_attr(get_option('lwbio_channel_text')); ?>">
+				</div>
+				<button type="submit" class="btn btn-primary">Salvar configurações</button>
+			</form>
+		</div>
 	</div>
 </div>
