352fc9f59789ccbbfc88fb3a0f690ebed4282f13
LeonardoWelter <leonardo.dias_silva@hotmail.com>
2022-05-31 17:46:57 -0300

    * Split DB Tables; * Add PHPDocBlock;

--- src/BioDatabase.php
+++ src/BioDatabase.php
@@ -2,159 +2,77 @@
 
 class BioDatabase {
 
+    /**
+     * Creates plugin database tables
+     * 
+     * Schemas:
+     * 
+     * links - id, ordem, text, url
+     * channels - id, ordem, url, service
+     *
+     * @return void
+     */
     public static function install() {
 
         global $wpdb;
-        $tableName = $wpdb->prefix . "lw_bio";
-
+        
         $charset_collate = $wpdb->get_charset_collate();
 
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
+        $tableName = $wpdb->prefix . "lwbio_links";
+
         $sql = "CREATE TABLE $tableName (
 		            id mediumint(9) NOT NULL AUTO_INCREMENT,
-		            time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+                    ordem tinyint NOT NULL,
                     text text NOT NULL,		
-                    url text DEFAULT '' NOT NULL,
-                    service text NOT NULL,
-                    type tinyint NOT NULL,
+                    url text NOT NULL,
                     PRIMARY KEY  (id)
 	            ) $charset_collate;";
+        dbDelta($sql);
 
+        $tableName = $wpdb->prefix . "lwbio_channels";
 
+        $sql = "CREATE TABLE $tableName (
+		            id mediumint(9) NOT NULL AUTO_INCREMENT,
+                    ordem tinyint NOT NULL,	
+                    url text NOT NULL,
+                    service text NOT NULL,
+                    PRIMARY KEY  (id)
+	            ) $charset_collate;";
         dbDelta($sql);
     }
 
+    /**
+     * Populates the tables on plugin initialization
+     *
+     * @return void
+     */
     public static function install_data() {
 
         global $wpdb;
-        $tableName = $wpdb->prefix . "lw_bio";
+        $tableLinks = $wpdb->prefix . "lwbio_links";
+        $tableChannels = $wpdb->prefix . "lwbio_channels";
 
-        $welcome_text = 'Meu primeiro botão';
+        $welcome_text = 'Site';
         $welcome_url = get_site_url();
 
         $wpdb->insert(
-            $tableName,
+            $tableLinks,
             array(
-                'time' => current_time('mysql'),
+                'ordem' => BioLinks::getMaxOrder() + 1,
                 'text' => $welcome_text,
                 'url' => $welcome_url,
-                'type' => 0,
-                'service' => 'link',
             )
         );
 
         $wpdb->insert(
-            $tableName,
+            $tableChannels,
             array(
-                'time' => current_time('mysql'),
-                'text' => 'channel',
+                'ordem' => BioChannels::getMaxOrder() + 1,
                 'url' => $welcome_url,
-                'type' => 1,
                 'service' => 'wordpress',
             )
         );
-    }
-
-    public static function index($type = 2) {
-
-        global $wpdb;
-        $tableName = $wpdb->prefix . "lw_bio";
-
-        if ($type == 0) {
-            $results = $wpdb->get_results("SELECT id, text, url, type, service FROM $tableName WHERE type = 0;", OBJECT);
-        } else if ($type == 1) {
-            $results = $wpdb->get_results("SELECT id, text, url, type, service FROM $tableName WHERE type = 1;", OBJECT);
-        } else if ($type == 2) {
-            $results = $wpdb->get_results("SELECT id, text, url, type, service FROM $tableName;", OBJECT);
-        }
-
-        return $results;
-    }
-
-    public static function show($id) {
-        global $wpdb;
-        $tableName = $wpdb->prefix . "lw_bio";
-
-        $result = $wpdb->get_row(
-            $wpdb->prepare(
-                "SELECT id, text, url, type, service
-               FROM $tableName
-               WHERE id = %d",
-                $id
-            )
-        );
-
-        return $result;
-    }
-
-    public static function store() {
-        global $wpdb;
-        $tableName = $wpdb->prefix . "lw_bio";
-
-        $link = $_POST;
-
-        $wpdb->insert(
-            $tableName,
-            array(
-                'text' => $link['lwbio_text'],
-                'url' => $link['lwbio_url'],
-                'type' => $link['lwbio_type'],
-                'service' => $link['lwbio_service'],
-                'time' => current_time('mysql')
-            ),
-            array(
-                '%s',
-                '%s',
-                '%d',
-                '%s',
-                '%s',
-            )
-        );
-
-        wp_redirect(admin_url('admin.php?page=lwbio'));
-        exit();
-    }
-
-    public static function update() {
-        global $wpdb;
-        $tableName = $wpdb->prefix . "lw_bio";
-
-        $link = $_POST;
-
-        $wpdb->update(
-            $tableName,
-            array(
-                'text' => $link['lwbio_text'],
-                'url' => $link['lwbio_url'],
-                'service' => $link['lwbio_service'],
-            ),
-            array('id' => $link['lwbio_id']),
-            array(
-                '%s',
-                '%s',
-                '%s',
-            ),
-            array('%d')
-        );
-
-        wp_redirect(admin_url('admin.php?page=lwbio'));
-        exit();
-    }
-
-    public static function destroy() {
-        global $wpdb;
-        $tableName = $wpdb->prefix . "lw_bio";
-
-        $link = $_POST;
-
-        $wpdb->delete(
-            $tableName,
-            array('id' => $link['lwbio_id']),
-            array('%d')
-        );
-
-        wp_redirect(admin_url('admin.php?page=lwbio'));
-        exit();
     }
 }
